b6d4eeafc729c4bd2f5cd2da41da04820f5398b9
RavinakS <ravinasingh697@gmail.com>
2026-07-08 07:48:42 +0530

    starting with authentication/register/login, updated tsconfiig file

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -15,6 +16,8 @@ import { UsersModule } from './users/users.module';
     MongooseModule.forRoot(process.env.MONGO_URI!),
 
     UsersModule,
+
+    AuthModule,
   ],
 
   controllers: [AppController],
--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -2,11 +2,14 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
 @Schema({ timestamps: true })
 export class User {
-  @Prop({ required: true })
-  name: string;
+  @Prop({ required: true, trim: true })
+  name!: string;
 
-  @Prop({ required: true, unique: true })
-  email: string;
+  @Prop({ required: true, unique: true, lowercase: true, trim: true })
+  email!: string;
+
+  @Prop({ required: true, select: false })
+  password!: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
